09af1dcb9225fd9699cfa5b8c2507cb130a53ad7
Karim Abou Zeid <karim23697@gmail.com>
2021-03-22 21:37:07 +0100

    Fix https://github.com/kabouzeid/nvim-lspinstall/issues/9

--- lua/lspinstall/servers/rome.lua
+++ lua/lspinstall/servers/rome.lua
@@ -3,8 +3,8 @@ require'lspconfig/configs'.rome = nil -- important, immediately unset the loaded
 config.default_config.cmd[1] = "./node_modules/.bin/rome"
 
 return vim.tbl_extend('error', config, {
-  install_script = [[
-  ! -f package.json && npm init -y --scope=lspinstall || true
+  install_script = [=[
+  [[ ! -f package.json ]] && npm init -y --scope=lspinstall || true
   npm install rome@latest
-  ]]
+  ]=]
 })
