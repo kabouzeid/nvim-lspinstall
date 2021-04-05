8b78dc4c0fb6632b50b3d7d787c247445865831e
Karim Abou Zeid <karim23697@gmail.com>
2021-04-05 20:01:13 +0200

    Remove @latest flags for go get since go versions <1.15 don't support it. It's also wasn't needed since @upgrade is the default flag.

--- lua/lspinstall/servers/efm.lua
+++ lua/lspinstall/servers/efm.lua
@@ -4,7 +4,7 @@ config.default_config.cmd[1] = "./efm-langserver"
 
 return vim.tbl_extend('error', config, {
   install_script = [[
-  GOPATH=$(pwd) GOBIN=$(pwd) GO111MODULE=on go get -v github.com/mattn/efm-langserver@latest
+  GOPATH=$(pwd) GOBIN=$(pwd) GO111MODULE=on go get -v github.com/mattn/efm-langserver
   GOPATH=$(pwd) GO111MODULE=on go clean -modcache
   ]]
 })
--- lua/lspinstall/servers/go.lua
+++ lua/lspinstall/servers/go.lua
@@ -4,7 +4,7 @@ config.default_config.cmd[1] = "./gopls"
 
 return vim.tbl_extend('error', config, {
   install_script = [[
-  GOPATH=$(pwd) GOBIN=$(pwd) GO111MODULE=on go get -v golang.org/x/tools/gopls@latest
+  GOPATH=$(pwd) GOBIN=$(pwd) GO111MODULE=on go get -v golang.org/x/tools/gopls
   GOPATH=$(pwd) GO111MODULE=on go clean -modcache
   ]]
 })
