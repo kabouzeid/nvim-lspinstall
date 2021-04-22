a28fd828eaabd0af4b3028e352988cce4e1c04b0
Karim Abou Zeid <karim23697@gmail.com>
2021-04-22 11:56:36 +0200

    Fix install for clangd 12.0.0. Fixes #41

--- lua/lspinstall/servers/cpp.lua
+++ lua/lspinstall/servers/cpp.lua
@@ -15,7 +15,7 @@ return vim.tbl_extend('error', config, {
   ;;
   esac
 
-  curl -L -o "clangd.zip" $(curl -s https://api.github.com/repos/clangd/clangd/releases/latest | grep 'browser_' | cut -d\" -f4 | grep "$platform")
+  curl -L -o "clangd.zip" $(curl -s https://api.github.com/repos/clangd/clangd/releases/latest | grep 'browser_' | cut -d\" -f4 | grep "clangd-$platform")
   rm -rf clangd
   unzip clangd.zip
   rm clangd.zip
