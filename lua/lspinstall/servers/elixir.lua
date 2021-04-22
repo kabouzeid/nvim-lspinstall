504ec288e54b596acb70255d599bbc7b1fbf1174
arcticlimer <vinigm.nho@gmail.com>
2021-04-22 17:57:34 -0300

    Change elixir-ls install script

--- lua/lspinstall/servers/elixir.lua
+++ lua/lspinstall/servers/elixir.lua
@@ -3,13 +3,9 @@ require'lspconfig/configs'.elixirls = nil -- important, immediately unset the lo
 
 return vim.tbl_extend('error', config, {
   install_script = [[
-    DOWNLOAD_URL=$(curl -s https://api.github.com/repos/elixir-lsp/elixir-ls/releases/latest | grep 'browser' | grep 'elixir-ls.zip' | cut -d '"' -f4)
-    curl -L -o elixir-ls.zip $DOWNLOAD_URL
+    curl -fLO https://github.com/elixir-lsp/elixir-ls/releases/latest/download/elixir-ls.zip
     unzip elixir-ls.zip
     chmod +x language_server.sh
     rm -f elixir-ls.zip
   ]],
-  default_config = {
-    cmd = { "./language_server.sh" }
-  }
 })
