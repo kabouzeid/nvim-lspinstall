96932ec908e957041e62ef173f3472a5b0127d8e
arcticlimer <vinigm.nho@gmail.com>
2021-04-22 19:14:10 -0300

    Add cmd entry and change elixir install script

--- lua/lspinstall/servers/elixir.lua
+++ lua/lspinstall/servers/elixir.lua
@@ -1,11 +1,13 @@
 local config = require'lspconfig'.elixirls.document_config
 require'lspconfig/configs'.elixirls = nil -- important, immediately unset the loaded config again
+config.default_config.cmd = { "elixir-ls/language_server.sh" }
 
 return vim.tbl_extend('error', config, {
   install_script = [[
+    rm -rf elixir-ls
     curl -fLO https://github.com/elixir-lsp/elixir-ls/releases/latest/download/elixir-ls.zip
-    unzip elixir-ls.zip
-    chmod +x language_server.sh
+    unzip elixir-ls.zip -d elixir-ls
+    chmod +x elixir-ls/language_server.sh
     rm -f elixir-ls.zip
   ]],
 })
