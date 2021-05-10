757a4fd4c0669eed93bccac0b822398ddaafebb9
Karim Abou Zeid <karim23697@gmail.com>
2021-05-10 15:22:02 +0200

    Fix elm

--- lua/lspinstall/servers/elm.lua
+++ lua/lspinstall/servers/elm.lua
@@ -1,6 +1,11 @@
 local config = require"lspinstall/util".extract_config("elmls")
 config.default_config.cmd[1] = "./node_modules/.bin/elm-language-server"
 
+-- we don't install these globally, their location will be resolved automatically form node_modules
+config.default_config.init_options.elmPath = nil
+config.default_config.init_options.elmFormatPath = nil
+config.default_config.init_options.elmTestPath = nil
+
 return vim.tbl_extend('error', config, {
   install_script = [[
   ! test -f package.json && npm init -y --scope=lspinstall || true
