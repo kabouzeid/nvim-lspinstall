1b43f88abf129e19017edb4dbec61e3eb2718bdc
Karim Abou Zeid <karim23697@gmail.com>
2021-07-10 16:38:12 +0200

    update csharp arguments, thanks to @ajoubert

--- lua/lspinstall/servers/csharp.lua
+++ lua/lspinstall/servers/csharp.lua
@@ -2,6 +2,12 @@ local config = require"lspinstall/util".extract_config("omnisharp")
 config.default_config.cmd = { "./omnisharp/run", "--languageserver" , "--hostPID", tostring(vim.fn.getpid()) }
 
 return vim.tbl_extend('error', config, {
+  on_new_config = function (new_config, new_root_dir)
+    if new_root_dir ~= nil then
+      table.insert(new_config.cmd, "-s")
+      table.insert(new_config.cmd, new_root_dir)
+    end
+  end,
   install_script = [[
   os=$(uname -s | tr "[:upper:]" "[:lower:]")
 
