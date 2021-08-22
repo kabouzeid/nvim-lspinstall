bd3adbb94f07ee12f7a721ec45a94e7825c32c66
Daniel M. Capella <polyzen@archlinux.org>
2021-08-22 15:53:38 -0400

    tailwindcss: Use npm package

--- lua/lspinstall/servers/tailwindcss.lua
+++ lua/lspinstall/servers/tailwindcss.lua
@@ -1,16 +1,9 @@
 local config = require"lspinstall/util".extract_config("tailwindcss")
-config.default_config.cmd[1] = "./tailwindcss-language-server"
+config.default_config.cmd[1] = "./node_modules/.bin/tailwindcss-language-server"
 
 return vim.tbl_extend('error', config, {
   install_script = [[
-  curl -L -o tailwindcss-intellisense.vsix $(curl -s https://api.github.com/repos/tailwindlabs/tailwindcss-intellisense/releases/latest | grep 'browser_' | cut -d\" -f4)
-  rm -rf tailwindcss-intellisense
-  unzip tailwindcss-intellisense.vsix -d tailwindcss-intellisense
-  rm tailwindcss-intellisense.vsix
-
-  echo "#!/usr/bin/env bash" > tailwindcss-language-server
-  echo "node \$(dirname \$0)/tailwindcss-intellisense/extension/dist/server/tailwindServer.js \$*" >> tailwindcss-language-server
-
-  chmod +x tailwindcss-language-server
-  ]],
+  ! test -f package.json && npm init -y --scope=lspinstall || true
+  npm install @tailwindcss/language-server
+  ]]
 })
