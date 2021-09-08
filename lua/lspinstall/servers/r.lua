4d8eb4a151863f7e630d5e9c7cbde9bd1a133d5b
aspeddro <pdesacastro@gmail.com>
2021-09-08 20:30:30 -0300

    install local server

--- lua/lspinstall/servers/r.lua
+++ lua/lspinstall/servers/r.lua
@@ -1,7 +1,11 @@
-local config = require'lspinstall/util'.extract_config('r_language_server')
+local util = require"lspinstall/util"
+local config = util.extract_config('r_language_server')
+local path = util.install_path('r')
+
+config.default_config.cmd = { 'R', '--slave', '-e', 'library(languageserver, lib.loc = "' .. path .. '"); languageserver::run()' }
+
+local eval_command = '\'install.packages("languageserver", repos = "http://cran.us.r-project.org", lib = "' .. path .. '")\''
 
 return vim.tbl_extend('error', config, {
-  install_script = [[
-    R -e 'install.packages("languageserver", repos="http://cran.us.r-project.org")'
-  ]]
+  install_script = 'R -e ' .. eval_command
 })
