c500458eef31263968088d876224b38093f14f68
Derek Tata <derekjtata@gmail.com>
2021-09-20 14:55:14 -0400

    Cleanup
    Move contents from downloaded file/dir, to base dir, and remove unneeded
    clutter

--- lua/lspinstall/servers/zig.lua
+++ lua/lspinstall/servers/zig.lua
@@ -1,32 +1,21 @@
 
 local config = require"lspinstall/util".extract_config("zls")
-config.default_config.cmd[1] = "./ziglang/zls"
+config.default_config.cmd[1] = "./zls"
 
 return vim.tbl_extend('error', config, {
   install_script = [[
 
     os=$(uname -s | tr "[:upper:]" "[:lower:]")
-
     case $os in
-      darwin)
-        platform="macos"
-        ;;
-      linux)
-        platform="linux"
-        ;;
-      windows)
-        platform="windows"
-        ;;
-      *)
-        echo "Unsupported platform: $os"
-        exit 1
-        ;;
-      esac
-
+      darwin) platform="macos" ;;
+      linux) platform="linux" ;;
+      windows) platform="windows" ;;
+      *) echo "Unsupported platform: $os"; exit 1 ;;
+    esac
     curl -L -o "zls.tar.xz" $(curl -s https://api.github.com/repos/zigtools/zls/releases/latest | grep 'browser_' | cut -d\" -f4 | grep "x86_64-$platform.tar.xz")
     tar -xvf zls.tar.xz
-    mv x86_64-$platform ziglang
-    rm zls.tar.xz
+    mv x86_64-$platform/* .
+    rm -r zls.tar.xz x86_64-$platform/
     exit 0
 
   ]]
